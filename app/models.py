46f4f3b75ced68a96e8d6df0c8e622d98d815346
WN-B <75706324+WN-B@users.noreply.github.com>
2021-12-07 15:40:58 +0100

    Creat registration form

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -26,8 +27,9 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __repr__(self):#tells Python how to print objects of this class, which is going to be useful for debugging
+    def __repr__(self):  # tells Python how to print objects of this class, which is going to be useful for debugging
         return '<User {}>'.format(self.username)
+
 
 @login.user_loader
 def load_user(id):
